b047ad341415958caa1852d225996ec58d3282f4
Parsa <63924414+Parsa76@users.noreply.github.com>
2024-01-07 11:41:48 +0100

    Feature to edit existing items added to the app.

---
+++ ToDO/NewItem/ViewModels/EditItemViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  EditItemViewModel.swift
+//  ToDO
+//
+//  Created by Parsa Keshavarz on 07.01.24.
+//
+
+import Foundation
---
+++ ToDO/NewItem/Views/EditCurrentItemView.swift
@@ -0,0 +1,18 @@
+//
+//  EditCurrentItemView.swift
+//  ToDO
+//
+//  Created by Parsa Keshavarz on 04.01.24.
+//
+
+import SwiftUI
+
+struct EditCurrentItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditCurrentItemView()
+}
