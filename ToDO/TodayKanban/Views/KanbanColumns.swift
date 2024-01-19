930c583f1920806615dbedcf083c5c83ee36b0ab
Parsa <63924414+Parsa76@users.noreply.github.com>
2024-01-19 01:43:21 +0100

    KananView has been refactored

--- ToDO/TodayKanban/Views/KanbanColumns.swift
+++ ToDO/TodayKanban/Views/KanbanColumns.swift
@@ -8,11 +8,54 @@
 import SwiftUI
 
 struct KanbanColumns: View {
+    @Binding var isTargeted :Bool
+    @EnvironmentObject var vm: TodayKanbanViewModel
+    let width: CGFloat
+    let height: CGFloat
+    let color: Color
+    var title: String
+    let items: [ItemModel]
+    let state: Int16
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: .top) {
+            RoundedRectangle(cornerRadius: 10)
+                .fill(isTargeted ? Color.gray.opacity(0.5): Color.theme.background)
+                .frame(width: width ,  height: height ,alignment: .top )
+                .background(Color.clear)
+            VStack {
+                KanbanColumnsHeadBox(color: color, title: title)
+                KanbonColumnListView(items: items)
+            }
+        }
+        .dropDestination(for: ItemModel.self) { items, location in
+            switch state {
+                case 0:
+                    vm.moveToTodo(items: items)
+                    return true
+                case 1:
+                    vm.moveToDoing(items: items)
+                return true
+                case 2:
+                    vm.moveToDone(items: items)
+                    return true
+                default:
+                    vm.moveToDone(items: items)
+                    return true
+            }
+            
+        } isTargeted: { isTargeted in
+            self.isTargeted = isTargeted
+        }
     }
 }
 
 #Preview {
-    KanbanColumns()
+    KanbanColumns(isTargeted: .constant(true) ,
+                  width: UIScreen.main.bounds.width / 4,
+                  height: UIScreen.main.bounds.height ,
+                  color: .blue ,
+                  title: "To Do" ,
+                  items: [] ,
+                  state: 0)
+        .environmentObject(TodayKanbanViewModel(dependencies: Dependencies()))
 }
--- ToDO/TodayKanban/Views/TodayKanbanView.swift
+++ ToDO/TodayKanban/Views/TodayKanbanView.swift
@@ -9,16 +9,17 @@ import SwiftUI
 
 struct TodayKanbanView: View {
     
-    
     @StateObject var vm : TodayKanbanViewModel
     @State private var isTodoTargeted = false
     @State private var isDoingTargeted = false
     @State private var isDoneTargeted = false
+    
     init(dependencies: Dependencies) {
         _vm = StateObject(wrappedValue: TodayKanbanViewModel(dependencies: dependencies))
     }
     var body: some View {
         let width = UIScreen.main.bounds.width / 3
+        let height = UIScreen.main.bounds.height
         VStack (alignment: .leading){
             Text("Today")
                 .font(.title)
@@ -27,63 +28,17 @@ struct TodayKanbanView: View {
             ScrollView {
                 VStack (spacing: 6) {
                     HStack (alignment : .top){
-                        ZStack(alignment: .top) {
-                            Rectangle()
-                                .fill(isTodoTargeted ? Color.gray.opacity(0.5): Color.theme.background)
-                                .frame(width: width ,  height: UIScreen.main.bounds.height ,alignment: .top )
-                                .background(Color.clear)
-                            VStack {
-                                KanbanColumnsHeadBox(color: .blue, title: "To do")
-                                KanbonColumnView(items: vm.toDoItems)
-                            }
-                        }
-                        .dropDestination(for: ItemModel.self) { items, location in
-                            vm.moveToTodo(items: items)
-                            return true
-                        } isTargeted: { isTargeted in
-                            isTodoTargeted = isTargeted
-                        }
-                        ZStack(alignment: .top) {
-                            Rectangle()
-                                .fill(isDoingTargeted ? Color.gray.opacity(0.5): Color.theme.background)
-                                .frame(width: width ,  height: UIScreen.main.bounds.height ,alignment: .top )
-                                .background(Color.clear)
-                            VStack {
-                                KanbanColumnsHeadBox(color: .yellow, title: "Doing")
-                                KanbonColumnView(items: vm.doingItems)
-                            }
-                        }
-                        .dropDestination(for: ItemModel.self) { items, location in
-                            vm.moveToDoing(items: items)
-                            return true
-                        }   isTargeted: { isTargeted in
-                                isDoingTargeted = isTargeted
-                        }
-                        ZStack(alignment: .top) {
-                            Rectangle()
-                                .fill(isDoneTargeted ? Color.gray.opacity(0.5): Color.theme.background)
-                                .frame(width: width ,  height: UIScreen.main.bounds.height ,alignment: .top )
-                                .background(Color.clear)
-                            VStack {
-                                KanbanColumnsHeadBox(color: .green, title: "Done")
-                                KanbonColumnView(items: vm.doneITems)
-                            }
-                        }
-                        .dropDestination(for: ItemModel.self) { items, location in
-                            vm.moveToDone(items: items)
-                            return true
-                        } isTargeted: { isTargeted in
-                            isDoneTargeted = isTargeted
-                   }
-                        
+                        KanbanColumns(isTargeted: $isTodoTargeted, width: width, height: height, color: .blue, title: "To do", items: vm.toDoItems, state: 0)
+                            .environmentObject(vm)
+                        KanbanColumns(isTargeted: $isDoingTargeted, width: width, height: height, color: .yellow, title: "Doing" , items: vm.doingItems, state: 1)
+                            .environmentObject(vm)
+                        KanbanColumns(isTargeted: $isDoneTargeted, width: width, height: height, color: .green, title: "Done" ,  items: vm.doneITems, state: 2)
+                            .environmentObject(vm)
                     }
                 }
-                
             }
-
         }
     }
-    
 }
 
 #Preview {
